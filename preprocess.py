33f7b8692437ed256cab8cc697b844b638dc9a87
Celal S. Turkmen <91391399+celaltrk@users.noreply.github.com>
2023-09-11 14:28:04 +0800

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,9 +1,6 @@
+# Import necessary libraries
 import os
 from glob import glob
-import shutil
-from tqdm import tqdm
-import numpy as np
-import nibabel as nib
 from monai.transforms import(
     Compose,
     AddChanneld,
@@ -18,76 +15,53 @@ from monai.transforms import(
 from monai.data import DataLoader, Dataset, CacheDataset
 from monai.utils import set_determinism
 
-def create_groups(in_dir, out_dir, Number_slices):
-    '''
-    This function is to get the last part of the path so that we can use it to name the folder.
-    `in_dir`: the path to your folders that contain dicom files
-    `out_dir`: the path where you want to put the converted nifti files
-    `Number_slices`: here you put the number of slices that you need for your project and it will 
-    create groups with this number.
-    '''
-
-    for patient in glob(in_dir + '/*'):
-        patient_name = os.path.basename(os.path.normpath(patient))
-
-        # Here we need to calculate the number of folders which mean into how many groups we will divide the number of slices
-        number_folders = int(len(glob(patient + '/*')) / Number_slices)
-
-        for i in range(number_folders):
-            output_path = os.path.join(out_dir, patient_name + '_' + str(i))
-            os.mkdir(output_path)
-
-            # Move the slices into a specific folder so that you will save memory in your desk
-            for i, file in enumerate(glob(patient + '/*')):
-                if i == Number_slices + 1:
-                    break
-                
-                shutil.move(file, output_path)
-
-def find_empty(in_dir):
-    '''
-    This function will help you to find the empty volumes that you may not need for your training
-    so instead of opening all the files and search for the empty ones, them use this function to make it quick.
-    '''
-    
-    list_patients = []
-    for patient in glob(os.path.join(in_dir, '*')):
-        img = nib.load(patient)
-
-        if len(np.unique(img.get_fdata())) > 2:
-            print(os.path.basename(os.path.normpath(patient)))
-            list_patients.append(os.path.basename(os.path.normpath(patient)))
-    
-    return list_patients
-
-
 def prepare(in_dir, pixdim=(1.5, 1.5, 2.0), a_min=-200, a_max=200, spatial_size=[128,128,64], cache=False):
+    '''
+    Prepares and returns data loaders for training and testing.
 
-    """
-    This function is for preprocessing, it contains only the basic transforms, but you can add more operations that you 
-    find in the Monai documentation.
-    https://monai.io/docs.html
-    """
+    Args:
+        in_dir (str): Directory containing input data.
+        pixdim (tuple, optional): Voxel dimensions (default: (1.5, 1.5, 2.0)).
+        a_min (float, optional): Minimum intensity for scaling (default: -200).
+        a_max (float, optional): Maximum intensity for scaling (default: 200).
+        spatial_size (list, optional): Spatial size for resizing (default: [128, 128, 64]).
+        cache (bool, optional): Whether to use cache for data loading (default: False).
 
+    Returns:
+        tuple: (train_loader, test_loader) - Data loaders for training and testing.
+    '''
+    
+    # Set determinism for reproducibility
     set_determinism(seed=0)
 
+    # Get file paths for training and testing images and labels
     path_train_images = sorted(glob(os.path.join(in_dir, 'imagesTr', "*.nii.gz")))
     path_train_labels = sorted(glob(os.path.join(in_dir, 'labelsTr', '*.nii.gz')))
     path_val_images = sorted(glob(os.path.join(in_dir, 'imagesVal', '*.nii.gz')))
     path_val_labels = sorted(glob(os.path.join(in_dir, 'labelsVal', '*.nii.gz')))
 
+    # Create a list of dictionaries specifying image-label pairs for training and testing  
     train_files = [{"image": image_name, "label": label_name} for image_name, label_name in zip(path_train_images, path_train_labels)]
     test_files = [{"image": image_name, "label": label_name} for image_name, label_name in zip(path_val_images, path_val_labels)]
 
+    # Define data transforms for training and testing
     train_transforms = Compose(
         [
+            # Load image and label data from keys "image" and "label"
             LoadImaged(keys=["image", "label"]),
+            # Add a channel dimension to the image and label tensors
             AddChanneld(keys=["image", "label"]),
+            # Rescale voxel spacings to (1.5, 1.5, 1.0) with bilinear interpolation for image, and nearest interpolation for label
             Spacingd(keys=["image", "label"], pixdim=pixdim, mode=("bilinear", "nearest")),
+            # Adjust the orientation of the image and label to anatomical standard "RAS"
             Orientationd(keys=["image", "label"], axcodes="RAS"),
+            # Scale image intensity values from the range [-200, 200] to [0.0, 1.0]
             ScaleIntensityRanged(keys=["image"], a_min=a_min, a_max=a_max, b_min=0.0, b_max=1.0, clip=True), 
+            # Crop the foreground region of the image and label based on the "image" key
             CropForegroundd(keys=["image", "label"], source_key="image"),
+            # Resize both the image and label to a spatial size of [128, 128, 64]
             Resized(keys=["image", "label"], spatial_size=spatial_size),   
+            # Convert image and label tensors to PyTorch tensors
             ToTensord(keys=["image", "label"]),
         ]
     )
@@ -106,6 +80,7 @@ def prepare(in_dir, pixdim=(1.5, 1.5, 2.0), a_min=-200, a_max=200, spatial_size=
     )
 
     if cache:
+        # Create cache-based datasets and data loaders
         train_ds = CacheDataset(data=train_files, transform=train_transforms,cache_rate=1.0)
         train_loader = DataLoader(train_ds, batch_size=1)
 
@@ -115,6 +90,7 @@ def prepare(in_dir, pixdim=(1.5, 1.5, 2.0), a_min=-200, a_max=200, spatial_size=
         return train_loader, test_loader
 
     else:
+        # Create non-cache-based datasets and data loaders
         train_ds = Dataset(data=train_files, transform=train_transforms)
         train_loader = DataLoader(train_ds, batch_size=1)
 
@@ -122,4 +98,3 @@ def prepare(in_dir, pixdim=(1.5, 1.5, 2.0), a_min=-200, a_max=200, spatial_size=
         test_loader = DataLoader(test_ds, batch_size=1)
 
         return train_loader, test_loader
-
